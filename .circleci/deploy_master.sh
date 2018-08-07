c2ef09724861ee7a4e0734b7b583b266064f2797
AamirMajeed290 <AamirMajeed290@gmail.com>
2018-08-07 12:03:25 +0100

    implement hockyapp

--- .circleci/deploy_master.sh
+++ .circleci/deploy_master.sh
@@ -2,5 +2,5 @@ curl \
 -F "status=2" \
 -F "notify=0" \
 -F "ipa=@AppName/MVPExample/build/outputs/apk/MVPExample-dev-debug.apk" \
--H "X-HockeyAppToken: ${HOCKEYAPP_APP_ID}" \
-https://rink.hockeyapp.net/api/2/apps/$AndroidAppId_Dev/app_versions/upload
+-H "X-HockeyAppToken: "9f8d07d7acb04e84b3f292b62b8ac0e0" \
+https://rink.hockeyapp.net/api/2/apps/MVPExample/app_versions/upload
