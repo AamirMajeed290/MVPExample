bce31e3623e3ed4440909f00d84c42aef0ae824c
AamirMajeed290 <AamirMajeed290@gmail.com>
2018-08-07 11:32:28 +0100

    implemnt CircleCi update 2

---
+++ app/src/main/java/example/code/mvpexample/view/Element.java
@@ -0,0 +1,15 @@
+package example.code.mvpexample.view;
+
+
+public class Element {
+    private String title;
+
+    public Element(String title) {
+        this.title = title;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+}
