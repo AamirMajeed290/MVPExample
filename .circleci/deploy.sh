b1833b22ce7542380a3f943fbb138f7faba30bfa
AamirMajeed290 <AamirMajeed290@gmail.com>
2018-08-07 15:05:33 +0100

    checking hockeyapp fixing

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,4 +3,4 @@ curl \
 -F "notify=0" \
 -F "ipa=@example.code.mvpexample/build/outputs/apk/example.code.mvpexample-dev-debug.apk" \
 -H "X-HockeyAppToken: 9f8d07d7acb04e84b3f292b62b8ac0e0" \
-https://rink.hockeyapp.net/api/2/apps/$freeWifi/app_versions/upload
+https://rink.hockeyapp.net/api/2/apps/freeWifi/app_versions/upload
